d8d29f721c4d139bd5bf55601b29e5a628f1bcae
Shi Zeng <shizeng@usc.edu>
2018-10-17 21:22:30 -0700

    fdfds

--- StepCounter/app/src/main/java/com/example/angelahe/stepcounter/Activity/DailyPlan.java
+++ StepCounter/app/src/main/java/com/example/angelahe/stepcounter/Activity/DailyPlan.java
@@ -1,10 +1,13 @@
 package com.example.angelahe.stepcounter.Activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -68,6 +71,25 @@ public class DailyPlan extends AppCompatActivity {
             }
         });
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.NavigationBar2);
+        bottomNavigationView.setSelectedItemId(R.id.AddMorePlan);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.Home:
+                        Intent intent = new Intent(DailyPlan.this, HomeActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.AddMorePlan:
+                        break;
+                    case R.id.Settings:
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
 
     class CustomerAdaptar extends BaseAdapter {
