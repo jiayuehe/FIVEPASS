016794072c393d14d4de34ed46210d1c300cda08
JiayueHe <jiayuehe@usc.edu>
2018-12-01 14:13:17 -0800

    Add Sending Message Functionality

--- StepCounter/app/src/main/java/com/example/angelahe/stepcounter/Activity/DailyPlan.java
+++ StepCounter/app/src/main/java/com/example/angelahe/stepcounter/Activity/DailyPlan.java
@@ -1,10 +1,17 @@
 package com.example.angelahe.stepcounter.Activity;
 
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +25,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.telephony.SmsManager;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -34,7 +42,8 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 public class DailyPlan extends AppCompatActivity {
-
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACT =0 ;
     private FloatingActionButton addExerciseButton;
     private String username;
     private List<Exercise> allExercise;
@@ -49,6 +58,11 @@ public class DailyPlan extends AppCompatActivity {
         username = intent.getStringExtra("username");
         allExercise = MainActivity.exerciseRoomDatabase.ExerciseDao().returnAllExercise(username);
 
+
+        if (ContextCompat.checkSelfPermission(DailyPlan.this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission();
+        }
         Log.d("What about here", "Is it here?");
         if (allExercise == null) {
             Log.d("CurrentExerciseisnull?", "No Exercise List");
@@ -136,6 +150,16 @@ public class DailyPlan extends AppCompatActivity {
             TextView textView = (TextView) convertView.findViewById(R.id.name);
             TextView textViewDes = (TextView) convertView.findViewById(R.id.startingDate);
             Button checkedButton = (Button) convertView.findViewById(R.id.check_button);
+            Button sendSMSBtn = (Button) convertView.findViewById(R.id.edit_button);
+
+            sendSMSBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Exercise currExercise = allExercise.get(position);
+                    String exerciseName = currExercise.getExerciseName();
+                    sendSMSMessage(exerciseName);
+                }
+            });
+
             Button videoButton = (Button) convertView.findViewById(R.id.video_button);
             videoButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -199,4 +223,60 @@ public class DailyPlan extends AppCompatActivity {
         intent.putExtra("username", username);
         startActivity(intent);
     }
+
+    protected void sendSMSMessage(String exerciseName) {
+        // TODO is the telephone number--Access the address book
+        String phoneNo = "8586994787";
+        String message = "Let us " + exerciseName + " together";
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), exerciseName+ " is sent as an invitation",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "SMS faild, please try again.",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void requestPermission() {
+        new AlertDialog.Builder(this).setTitle("Permission needed")
+                .setMessage("Allow usage to message your friend")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(DailyPlan.this,
+                                new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Permission Issue.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+
+    }
+
 }
